d34773df1e5e1c6fae1c30059eb81586b21ab62c
mzk <uta.mizuki@gmail.com>
2022-01-08 10:02:37 +0900

    TwitterCard修正

--- docs/js/common.js
+++ docs/js/common.js
@@ -55,6 +55,9 @@ function addTwitterCardMetaTags(url, title, description, image) {
         description = description.substring(0, 95) + "...";
     }
 
+    document.querySelector("meta[name='twitter:title']").setAttribute("content", title);
+    document.querySelector("meta[name='twitter:description']").setAttribute("content", description);
+    document.querySelector("meta[name='twitter:image']").setAttribute("content", image);
     document.querySelector("meta[property='og:url']").setAttribute("content", url);
     document.querySelector("meta[property='og:title']").setAttribute("content", title);
     document.querySelector("meta[property='og:description']").setAttribute("content", description);
